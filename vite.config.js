98bc794dbc62165d4b16a2af7c78d1ee7d3f926f
qvalfocus <qvalfocus@email.com>
2025-07-15 11:42:35 -0400

    setting up pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  root: 'src',               // since your index.html is in src
+  base: '/website/',         // replace "website" with your GitHub repo name
+  build: {
+    outDir: '../dist',       // tell Vite to output outside of src
+    emptyOutDir: true,
+  },
   plugins: [react()],
-})
+});
