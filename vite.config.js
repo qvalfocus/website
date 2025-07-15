7ee1cb4d33972b144a997a9f2b10478732de007d
qvalfocus <qvalfocus@email.com>
2025-07-15 12:05:28 -0400

    Set correct base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  root: 'src',               // since your index.html is in src
+export default defineConfig({               // since your index.html is in src
   base: '/website/',         // replace "website" with your GitHub repo name
-  build: {
-    outDir: '../dist',       // tell Vite to output outside of src
-    emptyOutDir: true,
-  },
   plugins: [react()],
 });
